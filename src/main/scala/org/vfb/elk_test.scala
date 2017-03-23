04e447b1de85e3d4decd2ef3a1105449b64099e5
David Osumi-Sutherland <dosumis@gmail.com>
2017-03-23 23:16:56 +0000

    Now a class a slightly random set of attributes and one method

--- src/main/scala/org/vfb/elk_test.scala
+++ src/main/scala/org/vfb/elk_test.scala
@@ -6,15 +6,26 @@ import org.semanticweb.owlapi.model.IRI
 import org.semanticweb.owlapi.util.SimpleShortFormProvider
 import org.semanticweb.owlapi.util.BidirectionalShortFormProviderAdapter
 import org.semanticweb.owlapi.search.EntitySearcher
+import org.semanticweb.owlapi.model.OWLClass
+import org.coode.owlapi.manchesterowlsyntax.ManchesterOWLSyntaxClassExpressionParser
 import scala.collection.JavaConversions._
 import java.io.File
+import java.util.Set
 
-object elk_test  {
+// see https://github.com/balhoff/test-elk/blob/master/src/main/scala/org/nescent/Main.scala
+// see also https://github.com/VirtualFlyBrain/VFB_owl/blob/master/src/code/mod/owl2pdm_tools.py 
+// for similar code in Jython using OWL-API v3
+
+case class elk_test(var iri_string: String) {
  
+  //TODO Turn this into a wrapper class for factory, manager, ontology etc
+  //Add some simple methods to return strings, map object etc as in owl2pdm_tools.py
+  // and Brain
+  
   val factory = OWLManager.getOWLDataFactory
   val manager = OWLManager.createOWLOntologyManager
-  //val ontology = manager.loadOntologyFromOntologyDocument(new File("/repos/drosophila-anatomy-developmental-ontology/fbbt/releases/fbbt-simple.owl"))
-  val ont_iri = IRI.create("http://purl.obolibrary.org/obo/fbbt/fbbt-simple.owl")
+//  val ontology = manager.loadOntologyFromOntologyDocument(new File("/repos/drosophila-anatomy-developmental-ontology/fbbt/releases/fbbt-simple.owl"))
+  val ont_iri = IRI.create(iri_string)
   val ontology = manager.loadOntology(ont_iri)
   
   val reasoner = new ElkReasonerFactory().createReasoner(ontology)
@@ -29,29 +40,39 @@ object elk_test  {
   
   val rNeuron_super = reasoner.getSuperClasses(rNeuron, false)
   
+
   
    //  Accessing the same class using short_form
    
   val simple_sfp = new SimpleShortFormProvider
-  val onts = collection.mutable.Set(ontology)
+  val onts = collection.mutable.Set(ontology) 
+  // scala.collection.JavaConversions._ magic takes care of casting onts 
+  // to appropriate java type for the following constructor:
   val bi_sfp = new BidirectionalShortFormProviderAdapter(manager, onts, simple_sfp)
   val e = bi_sfp.getEntity("FBbt_00003649")
+ 
+
+  def getSubClasses(short_form: String): Set[OWLClass]  = {
+    val e = bi_sfp.getEntity(short_form)
+    val c = e.asOWLClass
+    return this.reasoner.getSubClasses(c, false).getFlattened
+  }
   
-  
-  // How to get annotations? There is no longet a method 'getAnnotations' on OWLEntity in v4. Instead use:
-  
+  // How to get annotations? There is no longet a method 'getAnnotations' 
+  // on OWLEntity in v4. Instead use:
+
   val rNeuron_annotations = EntitySearcher.getAnnotations(e, ontology)
-  
+  //(see https://github.com/owlcs/owlapi/wiki/Migrate-from-version-3.4-and-3.5-to-4.0)  
   println(rNeuron_annotations.toString())
   
   for (a <- rNeuron_annotations) {
     println(a.toString())
   }
-  //(see https://github.com/owlcs/owlapi/wiki/Migrate-from-version-3.4-and-3.5-to-4.0)
   
   //get asserted superclasses:
   
-  val rNeuron_rel = EntitySearcher.getSuperClasses(rNeuron, ontology) //# Note - this only works with class object - rather than any old OWL entity.
+  val rNeuron_rel = EntitySearcher.getSuperClasses(rNeuron, ontology) 
+  //( only works with class object - rather than any old OWL entity.)
   
   for (r <- rNeuron_rel) {
     println(r.toString())
